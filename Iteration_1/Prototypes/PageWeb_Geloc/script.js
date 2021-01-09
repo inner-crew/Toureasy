08f8b860afb653436e6e846e28460e2bd9536a72
RDCMechanishesGerat <62136166+RDCMechanishesGerat@users.noreply.github.com>
2021-01-09 15:23:32 +0100

    Proto géoloc

---
+++ Iteration_1/Prototypes/PageWeb_Geloc/script.js
@@ -0,0 +1,27 @@
+var leBouton = document.querySelector("#b");
+var laCarte = document.querySelector("#b1");
+var divTexte = document.getElementById("message");
+var divBtnCarte = document.getElementById("ouvrirCarte");
+var pos;
+
+divBtnCarte.style.display = "none";
+
+leBouton.onclick = () => {
+    if ("geolocation" in navigator) {
+        /* la géolocalisation est disponible */
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var txt = "Position obtenu : (";
+            pos = position.coords.latitude + "/" + position.coords.longitude;
+            txt += pos + ")";
+            divTexte.innerHTML = "<p>" + txt + "</p>";
+            divBtnCarte.style.display = "block";
+        });
+      } else {
+        /* la géolocalisation n'est pas disponible */
+        alert("Problem guy's");
+      }
+}
+
+laCarte.onclick = () => {
+    window.location.href="https://www.openstreetmap.org/#map=20/" + pos;    
+}
