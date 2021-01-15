65868efb31afb403ce5f579d86e0f4cddd9f5f10
Nicolas <frache.nicolas@gmail.com>
2021-01-15 14:58:27 +0100

    Création dossier itération 2 et ajout diag de bdd dans itération 1

---
+++ Iteration_2/Base de données/triggers.sql
@@ -0,0 +1,27 @@
+
+
+ CREATE OR REPLACE TRIGGER trigger_contrib_moderateur
+ BEFORE INSERT OR UPDATE ON contribution
+ FOR EACH ROW
+ WHEN (new.moderateurDemande > 0)
+ DECLARE
+     V_ROLE_MODO MEMBRE.ROLE%type;
+	 V_PERM_Moderer ROLE.permModererContrib%type;
+ BEGIN
+     SELECT ROLE 
+	 INTO V_ROLE_MODO
+	 FROM MEMBRE
+	 WHERE IdMembre = :NEW.ModerateurDemande;
+	 
+	 SELECT permModererContrib 
+	 INTO V_PERM_Moderer 
+	 FROM ROLE
+	 WHERE idRole = V_ROLE_MOD;
+	 
+	 IF (  V_PERM_Moderer = FALSE )
+	 THEN
+		RAISE_APPLICATION_ERROR(-20010, 'L attribut moderateurDemande correspond à un membre qui n a pas la permission de moderer les contributions');
+	 END IF;
+	 
+
+ END;
