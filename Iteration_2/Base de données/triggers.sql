7306a59b94887084a74d6b95744efcf848a0848f
Nicolas <frache.nicolas@gmail.com>
2021-03-04 02:37:23 +0100

    Trigger de contribution qui vérifie les droits du moderateur (notamment)

--- Iteration_2/Base de données/triggers.sql
+++ Iteration_2/Base de données/triggers.sql
@@ -5,7 +5,7 @@ BEFORE INSERT ON `amis`
 FOR EACH ROW 
 BEGIN
 	DECLARE tmp INTEGER;
-	IF NEW.amis1 >= NEW.amis2 
+	IF NEW.amis1 > NEW.amis2 
 	THEN 
 		SET tmp := NEW.amis1;
 		SET NEW.amis1 = NEW.amis2;
@@ -20,38 +20,47 @@ END |
 DELIMITER ;
 
 
--- PAS AU POINT
- CREATE OR REPLACE TRIGGER trigger_contrib_moderateur
- BEFORE INSERT OR UPDATE ON contribution
- FOR EACH ROW
- WHEN (new.moderateurDemande > 0)
- DECLARE
-     V_ROLE_MODO MEMBRE.ROLE%type;
-	 V_PERM_Moderer ROLE.permModererContrib%type;
- BEGIN
-     SELECT ROLE 
-	 INTO V_ROLE_MODO
-	 FROM MEMBRE
-	 WHERE IdMembre = NEW.ModerateurDemande;
-	 
-	 SELECT permModererContrib 
-	 INTO V_PERM_Moderer 
-	 FROM ROLE
-	 WHERE idRole = V_ROLE_MOD;
-	 
-	 IF (  V_PERM_Moderer = FALSE )
-	 THEN
-		RAISE_APPLICATION_ERROR(-20010, 'L attribut moderateurDemande correspond à un membre qui n a pas la permission de moderer les contributions');
-	 END IF;
-	 
 
- END;
+DELIMITER |
+CREATE OR REPLACE TRIGGER `trigger_contrib_moderateur`
+BEFORE UPDATE ON `contribution`
+FOR EACH ROW
+BEGIN
+	
+	DECLARE V_ROLE_MODO tinyint(3) ;
+	DECLARE V_PERM_Moderer tinyint(1) ;
+	
 
+	IF ( NEW.statutContribution = 'acceptée' ) THEN
+		IF NEW.moderateurDemande IS NULL THEN
+			signal sqlstate '45000' set message_text = 'La contribution ne peut-être acceptée avec le champ moderateurDemande  à null';
+
+		ELSE 
+		
+			SELECT ROLE 
+				INTO V_ROLE_MODO
+				FROM MEMBRE
+				WHERE IdMembre = NEW.ModerateurDemande;
+			
+			SELECT permModererContrib 
+				INTO V_PERM_Moderer 
+				FROM ROLE
+				WHERE idRole = V_ROLE_MODO;
+			
+			IF (  V_PERM_Moderer = FALSE )
+			THEN
+				signal sqlstate '45000' set message_text = 'L attribut moderateurDemande correspond à un membre qui n a pas la permission de moderer les contributions';
+			END IF;
+			 
+		END IF;
+	END IF;
+END |
+DELIMITER ;
  
  
  
  
- 
+ INSERT INTO `contribution` (`idContribution`, `monumentTemporaire`, `monumentAModifier`, `contributeur`, `moderateurDemande`, `estNouveauMonument`, `date`, `statutContribution`, `description`) VALUES (NULL, '2', NULL, '1', '1', '1', now(), 'acceptée', NULL);
  
  
  
