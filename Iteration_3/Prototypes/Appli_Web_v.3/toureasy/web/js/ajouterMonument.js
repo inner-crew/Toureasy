01a4af1ac03eb36577eeb6fa9b69a30b42a77bd4
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2021-03-31 00:35:16 +0200

    Suite : prévisualisation

---
+++ Iteration_3/Prototypes/Appli_Web_v.3/toureasy/web/js/ajouterMonument.js
@@ -0,0 +1,37 @@
+window.onload = function ()
+{
+    document.getElementById("file-input").addEventListener("change", function() {
+        setGrid()
+        readURL(this);
+    })
+};
+
+var arrayFiles = []
+
+function setGrid() {
+    document.getElementById('galerie').className = "wrapper";
+    document.getElementById('smallImageUpload').style.width = '100%';
+}
+
+function readURL(input) {
+    var paras = document.getElementsByClassName('added');
+
+    while(paras[0]) {
+        paras[0].parentNode.removeChild(paras[0]);
+    }
+    for (var i = 0; i < input.files.length; i++) {
+        arrayFiles.push(input.files[i])
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            div = document.createElement("div")
+            div.className = "cell added"
+            img = document.createElement("img")
+            img.src = e.target.result
+            div.appendChild(img)
+            let galerie = document.getElementById('galerie');
+            let nbChild = galerie.childNodes.length
+            galerie.insertBefore(div, galerie.childNodes[nbChild-2])
+        }
+        reader.readAsDataURL(input.files[i]);
+    }
+}
