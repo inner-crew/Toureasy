5f7c0f96ea1570825a2c8910d6fb8713f9c1557e
RDCMechanishesGerat <remi.zapp9@etu.univ-lorraine.fr>
2021-03-31 18:29:33 +0200

    map giga update

--- Iteration_3/Prototypes/Appli_Web_v.3/toureasy/web/carteSetting/js/someFunction.js
+++ Iteration_3/Prototypes/Appli_Web_v.3/toureasy/web/carteSetting/js/someFunction.js
@@ -57,6 +57,88 @@ var belleMerSetting = {
         ]
     }
 };
+var batimentExtruction = {
+    'id': '3d-buildings',
+    'source': 'composite',
+    'source-layer': 'building',
+    'filter': ['==', 'extrude', 'true'],
+    'type': 'fill-extrusion',
+    'minzoom': 13,
+    'paint': {
+        'fill-extrusion-color': '#aaa',
+
+// use an 'interpolate' expression to add a smooth transition effect to the
+// buildings as the user zooms in
+        'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            13,
+            0,
+            15.05,
+            ['get', 'height']
+        ],
+        'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            13,
+            0,
+            15.05,
+            ['get', 'min_height']
+        ],
+        'fill-extrusion-opacity': 0.6
+    }
+}
+var clusterLayer = {
+    id: 'clusters',
+    type: 'circle',
+    source: 'monuments',
+    filter: ['has', 'point_count'],
+    paint: {
+        'circle-color': [
+            'step',
+            ['get', 'point_count'],
+            '#51bbd6',
+            100,
+            '#f1f075',
+            750,
+            '#f28cb1'
+        ],
+        'circle-radius': [
+            'step',
+            ['get', 'point_count'],
+            20,
+            100,
+            30,
+            750,
+            40
+        ]
+    }
+}
+var nbMonumentInClusterLayer = {
+    id: 'cluster-count',
+    type: 'symbol',
+    source: 'monuments',
+    filter: ['has', 'point_count'],
+    layout: {
+        'text-field': '{point_count_abbreviated}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': 12
+    }
+}
+var unMonumentLayer = {
+    id: 'unclustered-point',
+    type: 'symbol',
+    source: 'monuments',
+    filter: ['!', ['has', 'point_count']],
+    layout: {
+        'icon-image': ["get", "nomImage"],
+        'icon-size': 0.15,
+        'icon-allow-overlap': true,
+    },
+}
+
 
 var geolocOption = new mapboxgl.GeolocateControl({
     positionOptions: {enableHighAccuracy: true},
@@ -77,18 +159,34 @@ var rechercheDeLieuOption = new MapboxGeocoder({
 });
 
 var getImageUrlId = async function () {
-    return await getJsonFile("monumentPublique").then(jsonPublique => {
-        return getJsonFile(getCookie("token")).then(jsonPriver => {
-            let res = [];
-            jsonPublique.features.forEach(unMonumentPublique => {
-                res.push({url: ("../"+unMonumentPublique.properties.urlImage), id: unMonumentPublique.properties.nomImage})
-            });
-            jsonPriver.monumentsPrives.forEach(unMonumentPrive => {
-                res.push({url: ("../"+unMonumentPrive.urlImage), id: unMonumentPrive.nomImage})
-            });
-            return res;
-        });
+    let jsonPublique = await getJsonFile("monumentPublique")
+    let jsonPriver = await getJsonFile(getCookie("token"))
+    let res = [];
+    jsonPublique.features.forEach(unMonumentPublique => {
+        res.push({url: ("../" + unMonumentPublique.properties.urlImage), id: unMonumentPublique.properties.nomImage})
     });
+    jsonPriver.monumentsPrives.forEach(unMonumentPrive => {
+        res.push({url: ("../" + unMonumentPrive.urlImage), id: unMonumentPrive.nomImage})
+    });
+    return res;
+}
+
+var addImages = function(map, images) {
+    const addImage = (map, id, url) => {
+        return new Promise((resolve, reject) => {
+            map.loadImage(url, (error, image) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                map.addImage(id, image);
+                resolve(image);
+
+            });
+        });
+    }
+    const promises = images.map(imageData => addImage(map, imageData.id, imageData.url));
+    return Promise.all(promises);
 }
 
 var afficherCoordonner = function () {
@@ -106,14 +204,25 @@ var getJsonFile = function (nom) { //Revoir une promesse d'un fichier json
     if (nom === "monumentPublique") path = `../web/carteSetting/data/${nom}.json`;
     else path = `../web/carteSetting/data/tmp/${nom}.json`;
     return fetch(path)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                console.log('response error : ' +
+                    response.status + ' ' +
+                    response.statusText)
+            }
+        })
         .then(data => {
             return data;
+        })
+        .catch(error => {
+            console.log('network error : ' + error);
         });
 }
 
 //convertir un json de monuments de la database pour la rendre compatible avec l'API MapBox
-var convertirMonumentsEnFeature = function(jsonDeMonuments) {
+var convertirMonumentsEnFeature = function (jsonDeMonuments) {
     let res = {
         features: []
     };
@@ -155,7 +264,7 @@ var getCookie = function (cname) {
 }
 
 
-var majSelectBoxDesListes = function() {
+var majSelectBoxDesListes = function () {
     let selectBox = document.getElementById("monumentAfficher");
     selectBox.innerHTML = `<option value="publique" selected>Monuments certifiés</option>
   <option value="mesMonuments">Vos contribution</option>`
@@ -166,10 +275,132 @@ var majSelectBoxDesListes = function() {
     });
 }
 
+var onClickCluster = function (cluster, map) {
+    var features = map.queryRenderedFeatures(cluster.point, {
+        layers: ['clusters']
+    });
+    var clusterId = features[0].properties.cluster_id;
+    map.getSource('monuments').getClusterExpansionZoom(
+        clusterId,
+        function (err, zoom) {
+            if (err) {
+                return;
+            }
+            map.easeTo({
+                center: features[0].geometry.coordinates,
+                zoom: zoom
+            });
+        }
+    );
+}
+
+var onClickMonument = function (unClusteredPoint, map) {
+    var coordinates = unClusteredPoint.features[0].geometry.coordinates;
+// Ensure that if the map is zoomed out such that
+// multiple copies of the feature are visible, the
+// popup appears over the copy being pointed to.
+    while (Math.abs(unClusteredPoint.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += unClusteredPoint.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(
+            unClusteredPoint.features[0].properties.title
+        )
+        .addTo(map);
+}
+
+var quoiAfficherSurLaMap = function(e, map) {
+    switch (e.target.value) {
+        case ('publique') :
+            getJsonFile("monumentPublique").then(json => {
+                afficherMonument(json, map);
+            });
+            break;
+        case('mesMonuments') :
+            getJsonFile(getCookie("token")).then(json => {
+                let tmp = convertirMonumentsEnFeature(json.monumentsPrives);
+                let tmp2 = convertirMonumentsEnFeature(json.monumentsPubliques);
+                tmp2.features.forEach(unFeature => {
+                    tmp.features.push(unFeature);
+                })
+                afficherMonument(tmp, map);
+            });
+            break;
+        default :
+            let idDeLaListe = e.target.value;
+            getJsonFile(getCookie("token")).then(json => {
+                var lesMonuments = [];
+                var listesDesIdMonuments = [];
+                json.Listes.forEach(uneListe => {
+                    if (uneListe.liste.idListe.toString() === idDeLaListe.toString()) {
+                        uneListe.assosiation.forEach(uneAssociation => {
+                            listesDesIdMonuments.push(uneAssociation.idMonument);
+                        });
+                    }
+                });
+                let ttLesMonuments = json.monumentsPrives;
+                json.monumentsPubliques.forEach(unMonument => {
+                    ttLesMonuments.push(unMonument);
+                });
+                ttLesMonuments.forEach(unMonument => {
+                    listesDesIdMonuments.forEach(unId => {
+                        if (unId === unMonument.idMonument) lesMonuments.push(unMonument);
+                    });
+                });
+                afficherMonument(convertirMonumentsEnFeature(lesMonuments), map);
+            });
+            break;
+    }
+}
+
+var afficherMonument = function(json, map) {
+    map.getSource('monuments').setData(json);
+}
+
+var mapCharger = function (streetMap, sateliteMap) {
+    document.getElementById("3dSwitch").checked = false;
+    document.getElementById("3dSwitch").addEventListener("change", function () {
+        if (this.checked) {
+            streetMap.addLayer(batimentExtruction);
+            streetMap.setTerrain({'source': 'mapbox-dem', 'exaggeration': 1.5});
+            sateliteMap.setTerrain({'source': 'mapbox-dem', 'exaggeration': 1.5});
+        } else {
+            streetMap.removeLayer('3d-buildings');
+            streetMap.setTerrain(null);
+            sateliteMap.setTerrain(null);
+        }
+    });
+    getImageUrlId().then(data => {
+        addImages(streetMap, data).then(() => {
+            console.log("Image ajouter");
+        });
+    });
+
+    streetMap.addLayer(clusterLayer);
+    streetMap.addLayer(nbMonumentInClusterLayer);
+    streetMap.addLayer(unMonumentLayer);
+    streetMap.on('click', 'clusters', function (e) {
+        onClickCluster(e, streetMap)
+    });
+    streetMap.on('click', 'unclustered-point', function (e) {
+        onClickMonument(e, streetMap)
+    });
+    majSelectBoxDesListes();
+    document.getElementById("monumentAfficher").onchange = (e) => {
+        quoiAfficherSurLaMap(e, streetMap);
+    }
+    getJsonFile("monumentPublique").then(json => {
+        afficherMonument(json, streetMap);
+    });
+}
+
 
 export default {
     cielSetting: cielSetting,
     belleMerSetting: belleMerSetting,
+    batimentExtruction: batimentExtruction,
     premierFonction: premierFonction,
     geolocOption: geolocOption,
     rechercheDeLieuOption: rechercheDeLieuOption,
@@ -178,5 +409,6 @@ export default {
     getJsonFile: getJsonFile,
     convertirMonumentsEnFeature: convertirMonumentsEnFeature,
     getCookie: getCookie,
-    majSelectBoxDesListes: majSelectBoxDesListes
+    majSelectBoxDesListes: majSelectBoxDesListes,
+    mapCharger: mapCharger
 }
