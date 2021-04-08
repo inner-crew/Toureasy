a53c0a53fb6d9d0f1b7cd50aafc451be08bfd96a
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2021-04-08 20:56:43 +0200

    Ajout mention https dans les liens de partage

--- Iteration_3/Prototypes/Appli_Web_v.3/toureasy/src/controller/Controller.php
+++ Iteration_3/Prototypes/Appli_Web_v.3/toureasy/src/controller/Controller.php
@@ -195,7 +195,7 @@ class Controller
             if ($monument->estPrive == '0') {
                 if ($avoirUrl) {
                     $url = $this->c->router->pathFor('detail-monument', ['token' => $monument->token]);
-                    $partage = $_SERVER["HTTP_HOST"] . $this->c->router->pathFor('mapDetailMonument', ['token' => $monument->token]);
+                    $partage = "https://" . $_SERVER["HTTP_HOST"] . $this->c->router->pathFor('mapDetailMonument', ['token' => $monument->token]);
                     array_push($arrayMonumentsPublics, [$monument, $url, $partage]);
                 } else {
                     $monument->urlImage = $image->urlImage;
@@ -216,7 +216,7 @@ class Controller
             $image = Image::where('idMonument', '=', $monument->idMonument)->first();
             if ($avoirUrl) {
                 $url = $this->c->router->pathFor('detail-monument', ['token' => $monument->token]);
-                $partage = $_SERVER["HTTP_HOST"] . $this->c->router->pathFor('mapDetailMonument', ['token' => $monument->token]);
+                $partage = "https://" . $_SERVER["HTTP_HOST"] . $this->c->router->pathFor('mapDetailMonument', ['token' => $monument->token]);
                 array_push($arrayMonumentsPrives, [$monument, $url, $partage]);
             } else {
                 $monument->urlImage = $image->urlImage;
@@ -234,7 +234,7 @@ class Controller
         foreach ($listeDesListesUtilisateur as $liste) {
             if ($avoirUrl) {
                 $url = $this->c->router->pathFor('detail-liste', ['token' => $liste->token]);
-                $partage = $_SERVER["HTTP_HOST"] . $this->c->router->pathFor('mapDetailListe', ['token' => $liste->token]);
+                $partage = "https://" . $_SERVER["HTTP_HOST"] . $this->c->router->pathFor('mapDetailListe', ['token' => $liste->token]);
                 array_push($tabListes, [$liste, $url, $partage]);
             } else array_push($tabListes, $liste);
         }
@@ -1087,7 +1087,7 @@ class Controller
         }
         $tokenDemande = bin2hex(random_bytes(7));
 
-        $urlDemande = $_SERVER["HTTP_HOST"] . $this->c->router->pathFor("reception-demande-ami", ["token" => $tokenDemande]);
+        $urlDemande = "https://" . $_SERVER["HTTP_HOST"] . $this->c->router->pathFor("reception-demande-ami", ["token" => $tokenDemande]);
         echo '<script> console.log("' . $urlDemande . '"); </script>';
 
 
