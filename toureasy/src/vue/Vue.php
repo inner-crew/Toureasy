3486629f135d96d7ec378b5bfbe0a0bd049fe428
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2022-05-22 23:14:09 +0200

    fix

--- toureasy/src/vue/Vue.php
+++ toureasy/src/vue/Vue.php
@@ -179,10 +179,6 @@ END;
                                 </button>
                                 <button type="submit" name="action" value="Obtenir un code" class="mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
           <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            <!-- Heroicon name: solid/lock-closed -->
-            <svg class="h-5 w-5 text-yellow-500 group-hover:text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-            </svg>
           </span>
                                     Inscription
                                 </button>
